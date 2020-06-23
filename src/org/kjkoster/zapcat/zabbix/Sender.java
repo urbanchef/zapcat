938c35b57d64e6abdd16fa58c25059b164eacfc9
rmushchinskiy <rmushchinskiy@openwaygroup.com>
2020-06-23 14:00:29 +0300

    send header along with the message

--- src/org/kjkoster/zapcat/zabbix/Sender.java
+++ src/org/kjkoster/zapcat/zabbix/Sender.java
@@ -16,10 +16,7 @@ package org.kjkoster.zapcat.zabbix;
  * Zapcat. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.util.concurrent.BlockingQueue;
@@ -42,14 +39,6 @@ final class Sender extends Thread {
     private final InetAddress zabbixServer;
 
     private final int zabbixPort;
-
-    private static final String head = "<req><host>";
-
-    private static final String leftmiddle = "</host><key>";
-
-    private static final String rightmiddle = "</key><data>";
-
-    private static final String tail = "</data></req>";
 
     private boolean stopping = false;
 
@@ -118,26 +107,19 @@ final class Sender extends Thread {
             throws IOException {
         final long start = System.currentTimeMillis();
 
-        final StringBuilder message = new StringBuilder(head);
-        message.append(Base64.encode(host));
-        message.append(leftmiddle);
-        message.append(Base64.encode(key));
-        message.append(rightmiddle);
-        message.append(Base64.encode(value == null ? "" : value));
-        message.append(tail);
+        final String message = buildJSonString(host, key, value);
 
-        
-       log.finest("sending " + message);
+        log.finest("sending " + message);
 
         Socket zabbix = null;
-        OutputStreamWriter out = null;
+        OutputStream out = null;
         BufferedReader in = null;
         try {
             zabbix = new Socket(zabbixServer, zabbixPort);
             zabbix.setSoTimeout(TIMEOUT);
 
-            out = new OutputStreamWriter(zabbix.getOutputStream());
-            out.write(message.toString());
+            out = zabbix.getOutputStream();
+            writeMessage(out, message.getBytes());
             out.flush();
 
             in = new BufferedReader(new InputStreamReader(zabbix
@@ -163,5 +145,31 @@ final class Sender extends Thread {
         }
 
         log.info("send() " + (System.currentTimeMillis() - start) + " ms");
+    }
+
+    private String buildJSonString(String host, String item, String value) {
+        return 		  "{"
+                + "\"request\":\"sender data\",\n"
+                + "\"data\":[\n"
+                +        "{\n"
+                +                "\"host\":\"" + host + "\",\n"
+                +                "\"key\":\"" + item + "\",\n"
+                +                "\"value\":\"" + value.replace("\\", "\\\\") + "\"}]}\n" ;
+    }
+
+
+    protected void writeMessage(OutputStream out, byte[] data) throws IOException {
+        int length = data.length;
+
+        out.write(new byte[] {
+                'Z', 'B', 'X', 'D',
+                '\1',
+                (byte)(length & 0xFF),
+                (byte)((length >> 8) & 0x00FF),
+                (byte)((length >> 16) & 0x0000FF),
+                (byte)((length >> 24) & 0x000000FF),
+                '\0','\0','\0','\0'});
+
+        out.write(data);
     }
 }
